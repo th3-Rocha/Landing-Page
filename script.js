e6218ab394dce0714e73f9dea8cd67e1795bbbcd
th3-rocha <murilorocha537@gmail.com>
2022-07-28 19:11:31 -0300

    update title text

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ let scene,
   object,
   HEIGHT,
   WIDTH;
+let State;
 
-const createScene = () => {
+if(State == null){
+  State = "pyphone";
+}
+
+let createScene = () => {
   HEIGHT = window.innerHeight;
   WIDTH = window.innerWidth;
-
   scene = new THREE.Scene();
-
-
   aspectRatio = WIDTH / HEIGHT;
   fieldOfView = 10;
   nearPlane = 1;
@@ -27,24 +29,18 @@ const createScene = () => {
     nearPlane,
     farPlane
   );
-
   camera.position.x = 0;
   camera.position.z = 300;
   camera.position.y = 0;
-
   renderer = new THREE.WebGLRenderer({
     alpha: true,
     antialias: true
   });
   renderer.setSize(WIDTH, HEIGHT);
-
   renderer.shadowMap.enabled = true;
-
   container = document.getElementById("canvas");
   container.appendChild(renderer.domElement);
-
   window.addEventListener("resize", handleWindowResize, true);
-
   const geometry = new THREE.CircleGeometry( 5, 64 );
   const material = new THREE.MeshBasicMaterial( { color: 0x14F9E5 } );
   const circle = new THREE.Mesh( geometry, material );
@@ -52,18 +48,55 @@ const createScene = () => {
   circle.scale.set(2, 2, 2);
   circle.position.x = 30;
   circle.position.z = -30;
-
   let loader = new THREE.GLTFLoader();
-  loader.load( "gltf/PyPhone.gltf",
-    (gltf) => {
-      object = gltf.scene;
-      object.position.y = 5;
-      object.position.z = 0;
-      object.position.x = 25;
-      object.scale.set(6,6,6);
-      scene.add(object);
-    }
-  );
+  if(State == "pyphone"){
+    loader.load( "gltf/PyPhone.gltf",
+      (gltf) => {
+        object = gltf.scene;
+        object.position.y = 5;
+        object.position.z = 0;
+        object.position.x = 25;
+        object.scale.set(6,6,6);
+        scene.add(object);
+      }
+    );
+  }
+  else if (State == "pac"){
+    loader.load( "gltf/Pac.gltf",
+      (gltf) => {
+        object = gltf.scene;
+        object.position.y = 5;
+        object.position.z = 0;
+        object.position.x = 25;
+        object.scale.set(6,6,6);
+        scene.add(object);
+      }
+    );
+  }
+  else if (State == "pypad"){
+    loader.load( "gltf/PyPad.gltf",
+      (gltf) => {
+        object = gltf.scene;
+        object.position.y = 5;
+        object.position.z = 0;
+        object.position.x = 25;
+        object.scale.set(6,6,6);
+        scene.add(object);
+      }
+    );
+  }
+  else if (State == "pytv"){
+    loader.load( "gltf/PyTv.gltf",
+      (gltf) => {
+        object = gltf.scene;
+        object.position.y = 5;
+        object.position.z = 0;
+        object.position.x = 25;
+        object.scale.set(6,6,6);
+        scene.add(object);
+      }
+    );
+  }
 };
 
 
@@ -98,14 +131,48 @@ const loop = () => {
 };
 
 
+let stateFunction = (name) =>{
+  State = name;
+  console.log(name);
+}
 
+let main = () => {
+  var textOfTitle = document.getElementsByTagName("h3");
+  var textOfMain = document.getElementsByTagName("p");
+  if(State == "pyphone"){
+    textOfTitle[0].innerHTML = "Apresentando o <strong>PyPhone 4</strong>";
+    textOfMain[0].textContent = "Mais rapido";
+    textOfMain[1].textContent = "Biodegradável";
+    textOfMain[2].textContent = "Cabe na palma da mão";
+    textOfMain[3].textContent = "À prova d'água";
 
-const main = () => {
+  }else if (State == "pac"){
+    textOfTitle[0].innerHTML = "Apresentando o <strong>Pac Book Pro</strong>";
+    textOfMain[0].textContent = "Ultra Leve";
+    textOfMain[1].textContent = "Bateria dura semanas";
+    textOfMain[2].textContent = "Teclado orgânico";
+    textOfMain[3].textContent = "Ja vem com o Sistema Operacional PearOS";
+  }else if (State == "pypad"){
+    textOfTitle[0].innerHTML = "Apresentando o <strong>PyPad Max 2</strong>";
+    textOfMain[0].textContent = "Camera 4k";
+    textOfMain[1].textContent = "Ergonômico";
+    textOfMain[2].textContent = "Tela Super Vled";
+    textOfMain[3].textContent = "Utiliza Sistema Operacional PearOS Peartable";
+  }
+  else if (State == "pytv"){
+    textOfTitle[0].innerHTML = "Apresentando o <strong>PyTv Vision</strong>";
+    textOfMain[0].textContent = "Tela Ultra Luminosa";
+    textOfMain[1].textContent = "Som Sosurand 3";
+    textOfMain[2].textContent = "Tela TV Touch";
+    textOfMain[3].textContent = "Suporta 5G wireless";
+  }
+
+  // create and renders 3d objects
   createScene();
   createLights();
-
   renderer.render(scene, camera);
   loop();
+  // create and renders 3d objects
 };
 
 main();
